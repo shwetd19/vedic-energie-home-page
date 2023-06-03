f539963675d2097a1cc290b033b6a74556a281a2
shwetd19 <shwetas.dhake@mitaoe.ac.in>
2023-06-03 12:44:42 +0530

    updates made

--- src/Articles/articles.js
+++ src/Articles/articles.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./articles.css";
 import article_logo from "./article_logo.png";
+import address from "./address.png";
+import contact from "./contact.png";
+import mail from "./mail.png";
+
+import facebook from "./facebook.png";
+import linkedin from "./linkedin.png";
+import insta from "./insta.png";
+import twitter from "./twitter.png";
+import maps from "./maps.png";
+
 function Articles() {
   return (
     <section id="Articles" className="gradient-custom">
@@ -85,34 +95,59 @@ function Articles() {
             <p className="main_info">SOCIAL LINKS</p>
 
             <div className="icon_social">
-              <img src="/" alt="1" height="20" />
-              <img src="/" alt="1" height="20" />
-              <img src="/" alt="1" height="20" />
-              <img src="/" alt="1" height="20" />
+              <img src={facebook} alt="1" height="20" />
+              <img src={linkedin} alt="1" height="20" />
+              <img src={insta} alt="1" height="20" />
+              <img src={twitter} alt="1" height="20" />
             </div>
           </div>
 
           <div className="SOCIAL_LINKS_grid">
             <p className="main_info">SOCIAL LINKS</p>
             <ul>
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Testimonial</li>
-              <li>Blog</li>
-              <li>Our Team</li>
-              <li>Careers</li>
+              <a href="#"></a>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Testimonial</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Our Team</a>
+              </li>
+              <li>
+                <a href="#">Careers</a>
+              </li>
             </ul>
           </div>
 
           <div className="SOCIAL_LINKS_grid">
             <p className="main_info">SOCIAL LINKS</p>
             <ul>
-              <li>Energy Calculation</li>
-              <li>Energy Demand Management</li>
-              <li>Energy Supply Solution</li>
-              <li>Renewable Energy Solution</li>
-              <li>Reduce Energy Bills</li>
-              <li>Energy Saving</li>
+              <li>
+                <a href="#">Energy Calculation</a>
+              </li>
+              <li>
+                <a href="#">Energy Demand Management</a>
+              </li>
+              <li>
+                <a href="#">Energy Supply Solution</a>
+              </li>
+              <li>
+                <a href="#">Renewable Energy Solution</a>
+              </li>
+              <li>
+                <a href="#">Reduce Energy Bills</a>
+              </li>
+              <li>
+                <a href="#">Energy Saving</a>
+              </li>
             </ul>
           </div>
 
@@ -120,18 +155,20 @@ function Articles() {
             <p className="main_info">SOCIAL LINKS</p>
 
             <div className="cont">
-              <img src="/" alt="i" height="20px" /> Steeplebush Ave Unit 1
+              <img src={address} alt="i" height="20px" /> Steeplebush Ave Unit 1
               Brampton, ON L6R 3B4
             </div>
             <div className="cont">
-              <img src="/" alt="i" height="20px" />
+              <img src={mail} alt="i" height="20px" />
               @vedicenergie.com
             </div>
             <div className="cont">
-              <img src="/" alt="i" height="20px" />
+              <img src={contact} alt="i" height="20px" />
               +91 98439 37773
             </div>
-            <img src="/" alt="iii" height="80px" />
+            <div className="maps">
+              <img src={maps} alt="Maps" height="80px" />
+            </div>
           </div>
         </div>
 
